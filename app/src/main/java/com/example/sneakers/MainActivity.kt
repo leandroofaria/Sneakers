4b85dea229f762bb789dd6f1ba45f298b99168b8
Leandro Gabriel Faria <leandrogabrielfaria05@gmail.com>
2024-05-08 20:52:19 -0300

    Iniciando Repositorio

---
+++ app/src/main/java/com/example/sneakers/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.sneakers
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}

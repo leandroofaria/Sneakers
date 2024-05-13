90c7085e681a8197a0cd8d84f1eb620199ad66fe
Leandro Gabriel Faria <leandrogabrielfaria05@gmail.com>
2024-05-13 12:21:47 -0300

    Buscando as views com ID

--- app/src/main/java/com/example/sneakers/MainActivity.kt
+++ app/src/main/java/com/example/sneakers/MainActivity.kt
@@ -3,11 +3,21 @@ package com.example.sneakers
 import android.app.Activity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val nome = findViewById<TextView>(R.id.nome)
+        val valor = findViewById<TextView>(R.id.valor)
+        val descricao = findViewById<TextView>(R.id.descricao)
+
+        nome.text = "Cachorrooo"
+        valor.text = "Valor"
+        descricao.text = "DeSCRICAO"
+
+        
     }
 
 }
